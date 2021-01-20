68df9bc4f0d76e7cc3a620aaf353740393ba53d1
Ignacio Rojel <i.rojel@alumnos.duoc.cl>
2021-01-20 05:20:16 -0300

    Primer commit, con estructura general del proyecto

---
+++ components/Layout/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {styled} from '@emotion/styled';
+
+const Nav = styled.nav`
+    padding-left: 2rem;
+    min-width: 350px;
+    margin-top: .5rem;
+`;
+
+const Navigation = () => {
+    return (
+        
+        
+    )
+}
+
+export default Navigation
